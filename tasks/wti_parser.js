6afceb70f478cee6abdfc86aa0abf7514c0d68e2
Vladyslav Dovzhenko <vlad.dovzhenko@gmail.com>
2015-07-14 22:47:28 +0300

    update: checks if such locale was already created to prevent duplications

--- tasks/wti_parser.js
+++ tasks/wti_parser.js
@@ -16,6 +16,7 @@ module.exports = function (grunt) {
     najax = require('najax'),
     readline = require('readline'),
     q = require('q'),
+    path = require('path'),
     _ = require('lodash');
 
 
@@ -115,6 +116,37 @@ module.exports = function (grunt) {
     }
   }
 
+  function getAlreadyExistedTranslations(files, options) {
+    var queue = [];
+    var files = _.chain(files).filter({"master_project_file_id": null}).pluck('name').value();
+    console.log(files);
+    _.each(files, function(file) {
+      queue.push(
+        najax({
+          contentType: 'json',
+          url: 'https://webtranslateit.com/api/projects/' + options.apiKey + '/files/...?file_path=' + file,
+          type: 'GET'
+        })
+        .error(function (res) {
+          grunt.log.writeln('Error: downloading translation file:' + file);
+        })
+      );
+    })
+
+
+    return q.all(queue).then(function(responses) {
+      var locales = {};
+      _.each(responses, function(res, index) {
+        var obj = JSON.parse(res);
+        locales[files[index]] =  {
+          values: _.values(obj),
+          keys: _.keys(obj),
+        }
+      })
+      return locales;
+    });
+  }
+
   grunt.registerMultiTask('wtiParser', 'Grunt task for simplification adding of translations', function () {
     // Merge task-specific and/or target-specific options with these defaults.
     var options = this.options({
@@ -124,19 +156,62 @@ module.exports = function (grunt) {
         re: /\[\[(.+?)\]\]/gi // find [[placeholders]] in text,
       }),
       prev = this.async(),
+      baseLocale,
       file;
 
+    var locales;
+    wtiGetFiles(options, function(segmentFiles) {
+      baseLocale = _.find(segmentFiles, {"master_project_file_id": null}).locale_code;
+      console.log('Loading existed locales');
+      getAlreadyExistedTranslations(segmentFiles, options).then(function(existedLocales) {
+        console.log('looking for translation items')
+        prepareTranslations(existedLocales, this.files);
+        saveTranslations(segmentFiles);
+      }.bind(this))
+    }.bind(this))
 
-    function replaceHolders(content, dest, callback) {
+
+    function replaceHolders(content, dest, existedLocales,  callback) {
       function placeholderFactory(value, key, prev) {
+        var exists = false;
+        var existsIn = [];
+        _.each(existedLocales, function(localeFile, index) {
+          var valueIndex = localeFile.values.indexOf(key);
+          if (valueIndex != -1) {
+            existsIn.push({path: index + "::" + localeFile.keys[valueIndex], key: localeFile.keys[valueIndex], file: index});
+            exists = true;
+          }
+        })
         return function () {
-          rl.question('Enter key for "' + value + '": ', function (input) {
-            content = content.replace(value, options.prefix + (file.prefix ?  file.prefix + '.' : '') + input + options.postfix);
-
-            enterSegments(file, options, input, key.trim()).then(function (){
-              prev && prev();
+          if (exists) {
+            grunt.log.writeln('locale "' + key +  '" already exists in ' + existsIn.length + ' files:')
+            existsIn.forEach(function (file, i) {
+              grunt.log.writeln((i + 1) + ') file: ' + file.path);
             });
-          });
+            rl.question('Choose file or enter new key: ', function (input) {
+              var index = parseInt(input);
+              if (index != index) {
+                content = content.replace(value, options.prefix + (file.prefix ?  file.prefix + '.' : '') + input + options.postfix);
+                enterSegments(file, options, input, key.trim()).then(function (){
+                  prev && prev();
+                });
+              } else {
+                var filePrefix = path.basename(existsIn[index - 1].file, '.' + baseLocale + '.json');
+                content = content.replace(value, options.prefix + filePrefix + '.' + existsIn[index-1].key + options.postfix);
+                console.log('Locale ' + existsIn[index-1].path + ' was used for this item');
+                prev();
+              }
+            });
+
+          } else {
+            rl.question('Enter key for "' + value + '": ', function (input) {
+              content = content.replace(value, options.prefix + (file.prefix ?  file.prefix + '.' : '') + input + options.postfix);
+
+              enterSegments(file, options, input, key.trim()).then(function (){
+                prev && prev();
+              });
+            });
+          }
         };
       }
 
@@ -159,38 +234,43 @@ module.exports = function (grunt) {
 
     }
 
-    // Iterate over all specified file groups.
-    this.files.forEach(function (f) {
-      // Concat specified files.
-      f.src.filter(function (filepath) {
-        // Warn on and remove invalid source files (if nonull was set).
-        if (grunt.file.exists(filepath)) {
-          return true;
-        } else {
-          grunt.log.warn('Source file "' + filepath + '" not found.');
-          return false;
-        }
-      }).forEach(function (filepath) {
-        // Read file source.
-        prev = replaceHolders(grunt.file.read(filepath), f.dest, prev);
-      });
-    });
 
-    wtiGetFiles(options, proceedFiles.bind(null, function (_file_){
-      file = _file_;
-      var re = /(\w+)\.\w+\.json$/,
-        exec = re.exec(file.name);
-      if (file) {
-        if (exec && exec[1]) {
-          file.prefix = exec[1];
-          prev();
+    function prepareTranslations(existedLocales, files) {
+      // Iterate over all specified file groups.
+      files.forEach(function (f) {
+        // Concat specified files.
+        f.src.filter(function (filepath) {
+          // Warn on and remove invalid source files (if nonull was set).
+          if (grunt.file.exists(filepath)) {
+            return true;
+          } else {
+            grunt.log.warn('Source file "' + filepath + '" not found.');
+            return false;
+          }
+        }).forEach(function (filepath) {
+          // Read file source.
+          prev = replaceHolders(grunt.file.read(filepath), f.dest, existedLocales, prev);
+        });
+      });
+    }
+
+    function saveTranslations(files) {
+      proceedFiles(function (_file_){
+        file = _file_;
+        var re = /(\w+)\.\w+\.json$/,
+          exec = re.exec(file.name);
+        if (file) {
+          if (exec && exec[1]) {
+            file.prefix = exec[1];
+            prev();
+          } else {
+            grunt.log.writeln('Wrong file name in WTI, file name should be named by follow pattern "name.locale.json". Example "landing.en.json". RegExp: /(\w+)\.\w+\.json$/');
+          }
         } else {
-          grunt.log.writeln('Wrong file name in WTI, file name should be named by follow pattern "name.locale.json". Example "landing.en.json". RegExp: /(\w+)\.\w+\.json$/');
+          grunt.log.writeln('In your project no files or you use wrong API key');
         }
-      } else {
-        grunt.log.writeln('In your project no files or you use wrong API key');
-      }
-    }));
+      }, files)
+    }
 
   });
   grunt.registerMultiTask('wtiAddSegment', 'Grunt task for simplification adding of translations', function () {
